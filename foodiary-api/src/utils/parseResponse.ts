b3af09a87149109251a56d796fa17868873bb1f7
Diogo Ferreira <diogocraft.2@gmail.com>
2025-07-19 19:10:59 -0300

    feat: added apy gateway proxy response type, and command to remove infra

--- foodiary-api/src/utils/parseResponse.ts
+++ foodiary-api/src/utils/parseResponse.ts
@@ -1,6 +1,7 @@
+import { APIGatewayProxyResultV2 } from 'aws-lambda'
 import { HttpResponse } from "../types/http"
 
-export function parseResponse(response: HttpResponse) {
+export function parseResponse(response: HttpResponse): APIGatewayProxyResultV2 {
     return {
         statusCode: response.statusCode,
         body: JSON.stringify(response.body)
