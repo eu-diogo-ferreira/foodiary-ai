b6daf02c4589681281f8da8f44cdad8417faa709
Diogo Ferreira <diogocraft.2@gmail.com>
2025-07-20 12:23:13 -0300

    feat: aded meals list and record audio modal

--- foodiary/src/App.tsx
+++ foodiary/src/App.tsx
@@ -15,6 +15,8 @@ import { HomeHeader } from './components/home-header';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DateSwitcher } from './components/date-switcher';
 import { DailyStats } from './components/daily-stats';
+import { MealsList } from './components/meals-list';
+import { Home } from './screens/home';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -38,34 +40,7 @@ export default function App() {
   }
 
   return (
-    <View className="bg-white flex-1">
-      <SafeAreaProvider>
-        <HomeHeader />
-        <DateSwitcher />
-        <View className="mt-2">
-          <DailyStats
-            calories={{
-              current: 2000,
-              goal: 2500,
-            }}
-            proteins={{
-              current: 1500,
-              goal: 2500,
-            }}
-            carbohydrates={{
-              current: 1000,
-              goal: 2500,
-            }}
-            fats={{
-              current: 500,
-              goal: 2500,
-            }}
-          />
-        </View>
-
-        <View className="h-px bg-gray-200 mt-7" />
-      </SafeAreaProvider>
-    </View>
+    <Home />
   );
 }
 
