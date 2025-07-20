fe86cbc9880ed7fe85c2cb8688cc1ed7b63bc0ff
Diogo Ferreira <diogocraft.2@gmail.com>
2025-07-20 10:17:07 -0300

    feat: setup tailwind native

---
+++ foodiary/src/App.tsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk'
+
+import './styles/global.css'
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <View className="bg-lime-500 flex-1 items-center justify-center">
+      <Text className="text-base sans-regular">Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
